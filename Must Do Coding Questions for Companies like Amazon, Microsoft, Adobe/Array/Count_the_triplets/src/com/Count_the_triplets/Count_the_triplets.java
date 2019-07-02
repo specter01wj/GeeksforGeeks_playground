7d32ec26b52a94013c967ad191ca20fd98fcd606
specter01wj <specter01wj@gmail.com>
2019-07-02 00:19:24 -0400

    07/01/2019 v1

--- Must Do Coding Questions for Companies like Amazon, Microsoft, Adobe/Array/Count_the_triplets/src/com/Count_the_triplets/Count_the_triplets.java
+++ Must Do Coding Questions for Companies like Amazon, Microsoft, Adobe/Array/Count_the_triplets/src/com/Count_the_triplets/Count_the_triplets.java
@@ -17,23 +17,68 @@ public class Count_the_triplets {
 	public static void main(String[] args) {
 		int[] input = { 5, 32, 1, 7, 10, 50, 19, 21, 2 }; 
         int n = input.length; 
-        findTriplet(input, n); 
+        List<Integer> output = findTriplet(input, n); 
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
 	
 	/*
     solution:
-    The points of interest are the peaks and valleys in the given graph. 
-    We need to find the largest peak following the smallest valley. 
-    We can maintain two variables - minprice and maxprofit corresponding 
-    to the smallest valley and maximum profit (maximum difference between 
-    selling price and minprice) obtained so far respectively.
+    Simple approach: Run three loops and check if there exists a triplet such that 
+    sum of two elements equals the third element.
+	
+	Time complexity: O(n^3)
+	
+	Efficient approach : The idea is similar to Find a triplet that sum to a given value.
+	
+	Sort the given array first.
+	Start fixing the greatest element of three from back and traverse the array to find 
+	other two numbers which sum upto the third element.
+	Take two pointers j(from front) and k(initially i-1) to find the smallest of the two 
+	number and from i-1 to find the largest of the two remaining numbers
+	If the addition of both the numbers is still less than A[i], then we need to increase 
+	the value of the summation of two numbers, thereby increase the j pointer, so as to 
+	increase the value of A[j] + A[k].
+	If the addition of both the numbers is more than A[i], then we need to decrease the 
+	value of the summation of two numbers, thereby decrease the k pointer so as to 
+	decrease the overall value of A[j] + A[k].
     */
 	
 	/*
      * @param arr: a list of integers
-     * @return: void
+     * @return: list of three numbers such that sum of two elements 
+     * equals the third element
      */
-	
+	public static List<Integer> findTriplet(int[] arr, int n) { 
+        // sort the array 
+        Arrays.sort(arr); 
+        List<Integer> result = new ArrayList<Integer>();
+        
+        // for every element in arr 
+        // check if a pair exist(in array) whose 
+        // sum is equal to arr element 
+        for(int i = n - 1; i >= 0; i--) { 
+            int j = 0; 
+            int k = i - 1; 
+            
+            while(j < k) { 
+                if(arr[i] == arr[j] + arr[k]) { 
+  
+                    // pair found 
+                    result.add(arr[i]);
+                    result.add(arr[j]);
+                    result.add(arr[k]);
+  
+                    return result; 
+                } else if (arr[i] > arr[j] + arr[k]) {
+                    j += 1; 
+                } else {
+                    k -= 1;
+                }
+            } 
+        } 
+  
+        // no such triplet is found in array 
+        return result; 
+    }
 
 }
